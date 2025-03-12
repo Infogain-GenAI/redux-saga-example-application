9f0ed40cf9f310afa657ef47bc7b654ebc44606b
Harpreet Singh <H.Singh@igglobal.com>
2025-03-12 18:21:43 +0530

    reverted the change

--- src/components/loginSuccess/index.js
+++ src/components/loginSuccess/index.js
@@ -1,15 +1,16 @@
 import './index.less';
-import React, { Fragment } from 'react';
+import React,{Fragment} from 'react';
 import ActivityList from '../../components/activityList';
-
-export default LoginSuccess = (props) => {
-  const { toLoginOut, list } = props;
-  return (
-    <Fragment>
-      <div className="m-login-out">
-        <button onClick={toLoginOut}>Login out</button>
-      </div>
-      <ActivityList list={list} />
-    </Fragment>
-  )
+export default class LoginSuccess extends React.Component{
+  render(){
+    const {toLoginOut,list}=this.props;
+    return (
+     <Fragment>
+        <div className="m-login-out">
+           <button onClick={toLoginOut}>Login out</button>
+        </div>
+        <ActivityList list={list}/>
+     </Fragment>
+   )
+  }
 }
